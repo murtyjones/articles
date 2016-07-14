04f838a2f156ffc67bec390bc056aaeec4d0f565
marty jones <murtin.jones@gmail.com>
2016-07-14 09:47:43 -0400

    Tweaks

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Methods
-app.get('/', routes.index);
+app.get('/', routes.index;
 app.get('/articles', articles.list);
 app.get('/articles/add', articles.add);
 app.post('/articles/add', articles.save);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,3 @@
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Hello There. Not a lot to see here.' });
+  res.redirect('/articles');
 };
